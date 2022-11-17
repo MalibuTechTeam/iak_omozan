50443663930934a45d6ea2046e087087ebf595aa
Moldrok <alessio.aquilano@outlook.it>
2022-11-17 22:57:05 +0100

    v1.0.0

---
+++ iak_omozan_car/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Iakko(Malibù Tech Team)"
+description 'iak_omozan_car'
+
+version '1.0.0'
+
---
+++ iak_omozan_map/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Iakko(Malibù Tech Team)"
+description 'iak_omozan_map'
+
+version '1.0.0'
+
+this_is_a_map 'yes'
+
